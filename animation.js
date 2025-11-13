5332a8ef65769a1f7b93b0ec9a22d99dd2941094
kuna Hasada <kunahasada@gmail.com>
2025-11-13 15:09:59 +0530

    add animation

---
+++ animation.js
@@ -0,0 +1,30 @@
+gsap.from(".nav-left ul li",{
+    y:-100,
+    opacity:0,
+    duration:1,
+    stagger:0.3,
+})
+gsap.from(".nav-right ul li",{
+    y:-100,
+    opacity:0,
+    duration:1,
+    stagger:0.3,
+})
+
+gsap.from(".carousel-container",{
+    x:-200,
+    opacity:0,
+    duration:2,
+    stagger:0.3,
+})
+
+gsap.from(".menu-item",{
+    y:200,
+    opacity:0,
+    duration:1,
+    stagger:0.3,
+})
+
+
+
+
